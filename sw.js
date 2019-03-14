9f2ac4cd3bb9f32a602863228f9e26f5566b34b8
ajoykarmakar <32778214+ajoykarmakar@users.noreply.github.com>
2019-03-14 12:38:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,22 +2,38 @@ importScripts('/cache-polyfill.js');
 
 
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('bs').then(function(cache) {
-     return cache.addAll([
-       '/',
-       'index.html',
-       'index.html?homescreen=1',
-       '?homescreen=1',
-       '/build/main.css',
-       '/build/polyfills.js',
-       '/build/vendor.js',
-       '/build/main.js'
-     ]);
-   })
- );
+    e.waitUntil(
+        caches.open('bs').then(function(cache) {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'index.html?homescreen=1',
+                '?homescreen=1',
+                '/build/main.css',
+                '/build/polyfills.js',
+                '/build/vendor.js',
+                '/build/main.js'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-	console.log(event.request.url);
+    console.log(event.request.url);
+});
+
+self.addEventListener('fetch', function(event) {
+
+    console.log(event.request.url);
+
+    event.respondWith(
+
+        caches.match(event.request).then(function(response) {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
 });
