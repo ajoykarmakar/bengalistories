e3be8d31d37d3430529fffa661ca97da2085bf3d
ajoykarmakar <32778214+ajoykarmakar@users.noreply.github.com>
2019-03-14 12:35:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ self.addEventListener('install', function(e) {
      return cache.addAll([
        '/',
        'index.html',
+       'index.html?homescreen=1',
+       '?homescreen=1',
        '/build/main.css',
        '/build/polyfills.js',
        '/build/vendor.js',
