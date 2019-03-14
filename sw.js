e7cf192af252007987ffc47b78f6e9e908d90233
ajoykarmakar <32778214+ajoykarmakar@users.noreply.github.com>
2019-03-14 12:33:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+importScripts('/cache-polyfill.js');
 
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/build/main.css',
+       '/build/polyfills.js',
+       '/build/vendor.js',
+       '/build/main.js'
+     ]);
+   })
+ );
+});
